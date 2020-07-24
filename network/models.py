589c72b6dbbde7ed81ffd2702a0013686b786cbd
Nahin <mnk1@andrew.cmu.edu>
2020-07-24 23:46:43 +0300

    made relevant models for the project

--- network/models.py
+++ network/models.py
@@ -3,4 +3,11 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    followers = models.ManyToManyField("self", related_name="following")
+
+
+class Post(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
+    time_modified = models.TimeField(auto_now=True)
+    liking_users = models.ManyToManyField(User, related_name="liked_posts", blank=True)
